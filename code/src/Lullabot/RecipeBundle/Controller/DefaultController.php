7b52a023510aede597604675b0cf8592cedced99
Juampy <juampy72@gmail.com>
2013-04-28 17:16:37 +0300

    Added ingredients to recipe details, plus all the other fields.

--- code/src/Lullabot/RecipeBundle/Controller/DefaultController.php
+++ code/src/Lullabot/RecipeBundle/Controller/DefaultController.php
@@ -31,9 +31,18 @@ class DefaultController extends Controller
      */
     public function showAction($id)
     {
+      // Obtain recipe details.
       $client = new Client('http://copelandia.local');
       $request = $client->get('/node/' . $id . '.json');
       $recipe = $request->send()->json();
+
+      // Fetch ingredients.
+      $ingredients = array();
+      foreach ($recipe['field_recipe_ingredients'] as $ingredient) {
+        $request = $client->get('/field_collection_item/' . $ingredient['id'] .  '.json');
+        $ingredients[] = $request->send()->json();
+      }
+      $recipe['ingredients'] = $ingredients;
 
       return $this->render('LullabotRecipeBundle:Default:show.html.twig', array('recipe' => $recipe));
     }
