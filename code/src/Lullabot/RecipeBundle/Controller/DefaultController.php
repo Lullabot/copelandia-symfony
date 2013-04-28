3b2f85ffc75b22fc2a166890a86497d8ceb13b03
Juampy <juampy72@gmail.com>
2013-04-28 11:10:48 +0300

    Listing Recipes at the homepage: YAY!

--- code/src/Lullabot/RecipeBundle/Controller/DefaultController.php
+++ code/src/Lullabot/RecipeBundle/Controller/DefaultController.php
@@ -3,12 +3,24 @@
 namespace Lullabot\RecipeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Guzzle\Http\Client;
 
 class DefaultController extends Controller
 {
-    // Lists recipes.
+    /**
+     * Lists recipes.
+     */
     public function indexAction()
     {
-      return $this->render('LullabotRecipeBundle:Default:index.html.twig', array('name' => 'test'));
+      $client = new Client('http://copelandia.local');
+      $request = $client->get('/node.json');
+      $response = $request->send()->json();
+
+      $recipes = array();
+      foreach ($response['list'] as $recipe) {
+        $recipes[] = $recipe;
+      }
+
+      return $this->render('LullabotRecipeBundle:Default:index.html.twig', array('recipes' => $recipes));
     }
 }
