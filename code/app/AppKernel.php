07d58b3d0f5853637cea68c5c6d54f29050145b9
Juampy <juampy72@gmail.com>
2013-04-29 19:15:59 +0300

    Added dummy LullabotDrupalUser bundle which will handle user authentication.

--- code/app/AppKernel.php
+++ code/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Lullabot\RecipeBundle\LullabotRecipeBundle(),
+            new Lullabot\DrupalUserBundle\LullabotDrupalUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ code/src/Lullabot/DrupalUserBundle/LullabotDrupalUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lullabot\DrupalUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LullabotDrupalUserBundle extends Bundle
+{
+}
